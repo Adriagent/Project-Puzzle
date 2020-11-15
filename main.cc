f149c3b62669cecf193e76e2bcf8858480989eec
Adrian <adriansr2000@gmail.com>
2020-11-15 12:15:40 +0100

    añadido ia super guay

--- main.cc
+++ main.cc
@@ -4,12 +4,16 @@
 using namespace std;
 using namespace cv;
 int main(){
-
+    // + Doctor
     cout<<"Lena esta to buena\n";
     Mat pene=imread("lena.jpg");
     imshow("Imagen de Lena", pene);
-
+ // Erase una vez una caca cacota cacona que vivia en el campo
+ // rodeada de caca caquita cacuna 
     waitKey(0);
-jjjj
+
+    cout<<"Diego mola";
+
+
     return 0;
 }
